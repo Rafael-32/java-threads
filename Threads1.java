aecc8a097b42e1cd165dded324d8e4ab778213cf
Rafael-32 <76432805+Rafael-32@users.noreply.github.com>
2023-09-18 18:55:51 +0530

    git commit -m " updated "

--- Threads1.java
+++ Threads1.java
@@ -1,7 +1,7 @@
 public class Threads1 extends Thread{
     public void run(){
         int i=1;
-        while(i<10){
+        while(i<=10){
             System.out.println("Hello I am thread"+i);
             i++;
         }
